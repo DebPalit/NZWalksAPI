3e4d7f3eafc9948fa25350c415fe8f89cd4ef971
DebPalit <dpalit.official@gmail.com>
2026-03-26 13:02:31 +0530

    Add project files.

---
+++ NZWalksAPI/Controllers/EntityController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalksAPI.Controllers
+{
+    //test_controller url: http://localhost:5213/api/entity
+    //test_controller url: https://localhost:7083/api/entity
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EntityController : ControllerBase
+    {
+        [HttpGet(Name = "GetEntities")]
+        public IActionResult GetEntities()
+        {
+            string[] entities = { "Entity1", "Entity2", "Entity3", "Entity4", "Entity5" };
+
+            return Ok(entities);
+        }
+    }
+}
